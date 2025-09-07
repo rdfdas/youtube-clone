c961e597c603c77c0d37db0b244d4f759948d2a5
rdfdas <rafaeldfdasilva@yahoo.com>
2025-09-07 08:18:37 -0300

    Servidor Node com Express configurado

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-"console.log('Servidor inicial YouTube Clone');" 
+// Importando o Express
+const express = require("express");
+const path = require("path");
+
+// Inicializando o app
+const app = express();
+const PORT = 3000;
+
+// Servir arquivos estáticos (HTML, CSS, JS) da pasta atual
+app.use(express.static(path.join(__dirname)));
+
+// Rota principal
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// Iniciando servidor
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
+});
+
